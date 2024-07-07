bfeba50754bf51274022cb658e362a77ea059c11
KevinCasados <kevingeovanypor@gmail.com>
2024-07-07 10:23:05 -0600

    Page build, all changes

--- src/contexts/ProductContext/ProductContext.js
+++ src/contexts/ProductContext/ProductContext.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, {createContext, useState, useEffect} from 'react';
+import { json } from 'react-router-dom';
 
-const ProductContext = () => {
-  return <div>ProductContext</div>;
+// create context
+export const ProductContext = createContext();
+
+const ProductProvider = ({children}) => {
+  // products state
+  const [products, setProducts] = useState ([]);
+  //fecth products
+  useEffect(() => {
+    const fecthProducts = async ()=> {
+      const response = await fetch('https://fakestoreapi.com/products');
+      const data = await response.json();
+      setProducts(data);
+    };
+    fecthProducts();
+  },[]);
+
+
+  return (
+    <ProductContext.Provider value={{products}}>{children}</ProductContext.Provider>
+  )
 };
 
-export default ProductContext;
+export default ProductProvider;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// Product Provider
+import ProductProvider from './contexts/ProductContext/ProductContext';
+//sidebar Provider
+import SidebarProvider from './contexts/SidebarContext/SidebarContext';
+//cart Provider
+import CartProvider from './contexts/CartContext/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <SidebarProvider>
+    <CartProvider>
+      <ProductProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ProductProvider>
+    </CartProvider>
+  </SidebarProvider>
 );
